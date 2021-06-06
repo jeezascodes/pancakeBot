63e78efc36fbe1436f5ac2487089f869831f635f
Jesus gonzalez <jesus.gonzalez.xcv@gmail.com>
2021-06-05 22:04:24 -0500

    Add chalink abi in constants

--- get_chainlink_data_to_csv.py
+++ get_chainlink_data_to_csv.py
@@ -3,19 +3,12 @@ import csv
 from datetime import datetime
 from datetime import timedelta
 from optparse import OptionParser
-from constants import chainlink_addres
+from constants import chainlink_addres, chainlink_abi
 
 
 def get_chainlink_data_using_interval(min_t, max_t, seed=None, average=90):
     web3 = Web3(Web3.HTTPProvider('https://bsc-dataseed1.ninicoin.io/'))
-    abi = (
-        '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},'
-        '{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},'
-        '{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},'
-        '{"inputs":[{"internalType":"uint80","name":"_id","type":"uint80"}],"name":"getPreviousAnswer","outputs":[{"internalType":"int256","name":"price","type":"int256"}],"stateMutability":"view","type":"function"},'
-        '{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},'
-        '{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
-    )
+    abi = chainlink_abi
     addr = chainlink_addres
     contract = web3.eth.contract(address=addr, abi=abi)
 
