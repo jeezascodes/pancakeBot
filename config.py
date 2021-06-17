bd7e051a19f2fc43769cf99c39a62668c09d93b6
carlos huga <carlos.huga@gmail.com>
2021-06-17 10:29:21 -0500

    Config for default coqui

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 bot_max_bnb =  1.5
 bot_max_percentage = 0.2
 max_percentage_difference = 0.00171115983509094
-min_percentage_difference = 0.000
+min_percentage_difference = None
 round_duration = 300
 seconds_before_lock = 10
 chainlink_price_max_age = 70
-spearman_min_value = 0.8
+spearman_min_value = None
