9660df5aa179dcfb268c81be4a05793e6b462bab
carlos huga <carlos.huga@gmail.com>
2021-08-13 19:31:28 -0500

    set bet in a small number for default to avoid mistakes on pull

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Configurations
-bot_max_bnb =  1.5
-bot_max_percentage = 0.2
+bot_max_bnb =  0.05
+bot_max_percentage = 0.15
 max_percentage_difference = None
 min_percentage_difference = 0.00171115983509094
 round_duration = 300
